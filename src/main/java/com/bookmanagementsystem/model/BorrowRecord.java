efb0ead025f84628bc8677d56b7f98e2b9f210ba
manikanta569 <nannapanenimanikanta2@gmail.com>
2025-05-17 11:56:07 +0530

    intial commit

---
+++ src/main/java/com/bookmanagementsystem/model/BorrowRecord.java
@@ -0,0 +1,5 @@
+package com.bookmanagementsystem.model;
+
+public class BorrowRecord {
+
+}
--- src/main/java/com/bookmanagementsystem/model/Role.java
+++ src/main/java/com/bookmanagementsystem/model/Role.java
@@ -1,5 +1,5 @@
 package com.bookmanagementsystem.model;
 
-public class Books {
+public class Role {
 
 }
