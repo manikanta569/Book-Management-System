8028c7666880fa7181730676cac2622852548720
manikanta569 <nannapanenimanikanta2@gmail.com>
2025-05-19 12:41:40 +0530

    chnages done usermodel

--- src/main/java/com/bookmanagementsystem/model/User.java
+++ src/main/java/com/bookmanagementsystem/model/User.java
@@ -3,16 +3,20 @@ package com.bookmanagementsystem.model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "LibrarymanagmentSystem")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
-	private  String Name;
+	private String Name;
 	@Column(unique = true)
 	private String Email;
 	private String Password;
@@ -20,7 +24,7 @@ public class User {
 //	private String Role;
 	private LocalDateTime CreatedAt;
 	private LocalDateTime UpdatedAt;
-	
+
 	public User(Long id, String name, String email, String password, int phoneNumber, LocalDateTime createdAt,
 			LocalDateTime updatedAt) {
 		super();
@@ -32,50 +36,61 @@ public class User {
 		CreatedAt = createdAt;
 		UpdatedAt = updatedAt;
 	}
+
 	public Long getId() {
 		return Id;
 	}
+
 	public void setId(Long id) {
 		Id = id;
 	}
+
 	public String getName() {
 		return Name;
 	}
+
 	public void setName(String name) {
 		Name = name;
 	}
+
 	public String getEmail() {
 		return Email;
 	}
+
 	public void setEmail(String email) {
 		Email = email;
 	}
+
 	public String getPassword() {
 		return Password;
 	}
+
 	public void setPassword(String password) {
 		Password = password;
 	}
+
 	public LocalDateTime getCreatedAt() {
 		return CreatedAt;
 	}
+
 	public void setCreatedAt(LocalDateTime createdAt) {
 		CreatedAt = createdAt;
 	}
+
 	public LocalDateTime getUpdatedAt() {
 		return UpdatedAt;
 	}
+
 	public void setUpdatedAt(LocalDateTime updatedAt) {
 		UpdatedAt = updatedAt;
 	}
+
 	public int getPhoneNumber() {
 		return PhoneNumber;
 	}
+
 	public void setPhoneNumber(int phoneNumber) {
 		PhoneNumber = phoneNumber;
 	}
-	
-	
-	
 
 }
